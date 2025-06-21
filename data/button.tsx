e1bca42f1387e9b3d5004a022355bb61f552ee6d
Ernesto Pariona <eparionad@gmail.com>
2025-06-20 21:37:08 -0500

    add data in data.tsx

--- data/button.tsx
+++ data/button.tsx
@@ -18,12 +18,12 @@ export const iconButton: IconButtons = {
   calendarData: {
     title: 'Agregar a Google Calendar',
     icon: <Calendar />,
-    url: 'https://react-icons.github.io/react-icons/icons/lu/',
+    url: 'https://calendar.google.com/calendar/render?action=TEMPLATE&text=¡Nos%20casamos!%20Tatiana%20y%20Salvador&dates=20250802T103000/20250802T223000&details=Matrimonio%20de%20Tatiana%20y%20Salvador&location=Sumacc%20Huayta%20-%20Jr.%20Loreto%201655%2C%20Huancayo',
   },
   addressData: {
     title: 'Ver ubicación',
     icon: <Address />,
-    url: 'https://react-icons.github.io/react-icons/icons/lu/',
+    url: 'https://www.google.com/maps?q=-12.076517,-75.220859',
   },
   assistanceData: {
     title: 'Confirma aquí',
@@ -33,6 +33,6 @@ export const iconButton: IconButtons = {
   whatsappData: {
     title: 'Clic aquí',
     icon: <Whatsapp />,
-    url: 'https://react-icons.github.io/react-icons/icons/lu/',
+    url: 'https://wa.me/51962338129',
   },
 };
